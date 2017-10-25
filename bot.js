ab93aa70ec13292bf38ea1574bb4f0f8ca72de7a
mattmiller64 <miller.matthew64@gmail.com>
2017-10-24 19:06:10 -0500

    updated the calls - TODO fill out the db calls

--- bot.js
+++ bot.js
@@ -1,6 +1,8 @@
 //import { ScoreService } from "./services/score.service";
 var LadderService = require('./services/ladder.service');
 var ScoreService = require('./services/score.service');
+var CurrentInhouseService = require('./services/currentinhouse.service');
+
 var Discord = require('discord.js');
 const bot = new Discord.Client();
 var logger = require('winston');
@@ -53,8 +55,27 @@ bot.on("message", (message) => {
         LadderService.topForty(message);
     }
     //CurrentInhouseService
-    else if (message.content.startsWith(prefix + 'signUp')) {       //signs a user up for this days inhouse
-
+    else if (message.content.startsWith(prefix + 'signUp')) {           //signs a user up for this days inhouse
+        CurrentInhouseService.signUp(message);
+    }
+    else if (message.content.startsWith(prefix + 'endSignUps')) {       // this will also stop sign ups - if a team doesnt have 10 players, the team will disband
+        CurrentInhouseService.endSignUps(message);
+    }
+    else if (message.content.startsWith(prefix + 'startSignUps')) {     // opens the sign ups for the current in-houses today
+        CurrentInhouseService.startSignUps(message);
+    }
+    else if (message.content.startsWith(prefix + 'reOpenSignUps')) {    // Re-opens the sign ups to allow last minute people to sign up
+        CurrentInhouseService.reOpenSignUps(message);
+    }
+    //not sure how yet
+    else if (message.content.startsWith(prefix + 'makeWholeTeam')) {    // allows a user to make a whole team of pre-defined 5 players
+        CurrentInhouseService.makeWholeTeam(message);
+    }
+    else if (message.content.showTeams(prefix + 'showTeams')) {         // shows the list of current teams full or incomplete
+        CurrentInhouseService.showTeams(message);
+    }
+    else if (message.content.showTeams(prefix + 'endInhouse')) {        // ends the in-house games for the day
+        CurrentInhouseService.endInhouse(message);
     }
 });
 
--- services/currentinhouse.service.js
+++ services/currentinhouse.service.js
@@ -2,8 +2,12 @@ const sql = require("sqlite");
 sql.open("./db/inhouseDB.sqlite");
 //keeps track of teams for a certain day
 
-module.exports = class LadderService {
-    static addUser(message) {
+module.exports = class CurrentInHouseService {
+    //user signs up, if not added to ladder database throws error.
+    // if not added to ladder database throws warning
+    // if signups arent open, throws warning
+    // groups all sign ups in groups of ten and makes team even
+    static signUp(message) { 
         sql.get(`SELECT * FROM CurrentInHouse WHERE userId ="${message.author.id}"`).then(row => {
             if (!row) {
                 sql.run("INSERT INTO CurrentInHouse (userId, points, level) VALUES (?, ?, ?)", [message.author.id, 1, 0]);
@@ -24,17 +28,72 @@ module.exports = class LadderService {
                 });
             });
     }
-    static getLevel(message) {
+    // starts the sign ups for the current in-houses
+    static startSignUps(message) {
         sql.get(`SELECT * FROM CurrentInHouse WHERE userId ="${message.author.id}"`).then(row => {
             if (!row) return message.reply("Your current level is 0");
             message.reply(`Your current level is ${row.level}`);
             console.log(row);
         });
     }
-    static getPoints(message) {
+    //this will also stop sign ups - if a team doesnt have 10 players, the team will disband
+    static endSignUps(message) {
+        sql.get(`SELECT * FROM CurrentInHouse WHERE userId ="${message.author.id}"`).then(row => {
+            if (!row) return message.reply("Your current level is 0");
+            message.reply(`Your current level is ${row.level}`);
+            console.log(row);
+        });
+    }
+    //Re-opens the sign ups to allow last minute people to sign up
+    static reOpenSignUps(message) {
+        sql.get(`SELECT * FROM CurrentInHouse WHERE userId ="${message.author.id}"`).then(row => {
+            if (!row) return message.reply("Your current level is 0");
+            message.reply(`Your current level is ${row.level}`);
+            console.log(row);
+        });
+    }
+    //called by another function to create all sets of teams? - have this auto make once 10 people are hit
+    static makeTeams(message) {
+
+    }
+    //creates a team of 5 somehow
+    static makeWholeTeam(message) {
         sql.get(`SELECT * FROM CurrentInHouse WHERE userId ="${message.author.id}"`).then(row => {
             if (!row) return message.reply("sadly you do not have any points yet!");
             message.reply(`you currently have ${row.points} points, good going!`);
         });
+    }
+    //shows the list of current teams full or incomplete
+    static showTeams(message) {
+        sql.get(`SELECT * FROM CurrentInHouse WHERE userId ="${message.author.id}"`).then(row => {
+            if (!row) return message.reply("sadly you do not have any points yet!");
+            message.reply(`you currently have ${row.points} points, good going!`);
+        });
+    }
+    // ends the in-house for the day
+    static endInhouse(message) {
+        sql.get(`SELECT * FROM CurrentInHouse WHERE userId ="${message.author.id}"`).then(row => {
+            if (!row) return message.reply("sadly you do not have any points yet!");
+            message.reply(`you currently have ${row.points} points, good going!`);
+        });
+    }
+    //either average the number of points/ start from the highest and divy them out ie 2 diamonds, one on each team, 
+    //3 diamonds, 2 on one team, next highest on the other until the points even out or 5 are added
+    /*
+        ie 5 diamonds and 3 golds and 2 bronze
+        3 diamond on one team
+        2 diamond on other, then puts 3 golds on this
+        2 bronze on other
+        challenger = 8 pts
+        masters = 6 points
+        diamond = 5 points
+        plat = 4
+        gold = 3
+        silver = 2
+        bronze = 1
+        unranked = 3
+    */
+    static balanceTeams(){
+
     }
 }
